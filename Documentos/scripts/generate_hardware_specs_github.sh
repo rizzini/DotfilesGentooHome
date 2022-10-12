33380ff85a69777c03b9a760b82cdfad6f160158
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-10-12 09:37:30 -0300

    atualizando

--- Documentos/scripts/generate_hardware_specs_github.sh
+++ Documentos/scripts/generate_hardware_specs_github.sh
@@ -4,7 +4,7 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 export LANG=C LC_ALL=C;
-/usr/bin/fdisk -l | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/fdisk -l'
+/sbin/fdisk -l | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/fdisk -l'
 /usr/bin/glxinfo -B | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/glxinfo -B'
 /usr/bin/hwinfo | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/hwinfo'
 /usr/bin/inxi -F | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/inxi -F'
@@ -15,11 +15,10 @@ export LANG=C LC_ALL=C;
 /usr/bin/lsusb -v | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/lsusb -v'
 /usr/bin/vainfo | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/vainfo'
 /usr/bin/vulkaninfo | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/vulkaninfo'
-/usr/bin/btrfs device usage / | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/btrfs device usage'
-/usr/bin/btrfs filesystem df / | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/btrfs filesystem df'
-/usr/bin/btrfs filesystem show | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/btrfs filesystem show'
-/usr/bin/btrfs filesystem usage / | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/btrfs filesystem usage'
+/sbin/btrfs device usage / | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/btrfs device usage'
+/sbin/btrfs filesystem df / | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/btrfs filesystem df'
+/sbin/btrfs filesystem show | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/btrfs filesystem show'
+/sbin/btrfs filesystem usage / | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/btrfs filesystem usage'
 /usr/bin/sudo -u lucas /usr/bin/yay -Q | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/installed_package_list'
-/usr/bin/pacman -Qqem | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/installed_AUR_package_list'
-/usr/bin/mount | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/mount'
-/usr/bin/expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | /usr/bin/sort | /usr/bin/tail -200 | /usr/bin/nl | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/recently_installed_package_list'
+/bin/mount | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/mount'
+/usr/bin/eix-installed-after -dve0 | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/installed_package_list_by_date'
