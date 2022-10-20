254f26ac29e06a7d0ca6cc4cfcc2825974127ba5
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-10-20 07:50:30 -0300

    atualizando

--- Documentos/scripts/cpu_gpu_temp_taskbar.sh
+++ Documentos/scripts/cpu_gpu_temp_taskbar.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 export LANG=C LC_ALL=C;
-if [ "$1" == 'click' ]; then
-#     if /usr/bin/pgrep easyeffects; then
-#         /usr/bin/easyeffects;
-#     else
-        alacritty -o window.dimensions.lines=13 window.dimensions.columns=55 -e /usr/bin/sudo /usr/bin/intel_gpu_top
-#     fi
-fi
 if /bin/grep -q 'ENABLED=no' /etc/ufw/ufw.conf; then
     /bin/echo 'Firewall desativado';
 elif [ "$(pgrep easyeffects)" ]; then
     /usr/bin/printf "EasyEffects ligado | CPU: ""$(/bin/echo "$(/usr/bin/sensors | /bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)")""ºc";
 else
-    if [[ "$(/usr/bin/sensors | /bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)" -ge 65 || "$1" == 'taskbar' ]]; then
+    if [[ "$(/usr/bin/sensors | /bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)" -ge 65 || "$1" == 'always_show' ]]; then
         /usr/bin/printf "CPU: ""$(/bin/echo "$(/usr/bin/sensors | /bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)")""ºc";
     elif [ "$(/usr/bin/sensors | /bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)" == '00' ]; then
         /usr/bin/printf "CPU: ""$(/usr/bin/sensors | /bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)""ºc";
--- Documentos/scripts/disk_monitor_taskbar.sh
+++ Documentos/scripts/disk_monitor_taskbar.sh
@@ -19,8 +19,8 @@ fi
 /usr/bin/sleep 0.5 &&
 data2_read_sda2=$(/bin/grep -w sda /proc/diskstats | /usr/bin/awk '{print $6}');
 data2_write_sda2=$(/bin/grep -w sda /proc/diskstats | /usr/bin/awk '{print $10}');
-read_sda2=$((data2_read_sda2 - data1_read_sda2));
-write_sda2=$((data2_write_sda2 - data1_write_sda2));
+read_sda=$((data2_read_sda2 - data1_read_sda2));
+write_sda=$((data2_write_sda2 - data1_write_sda2));
 if [ "$sdb" ]; then
     data2_read_sdb=$(/bin/grep -w sdb /proc/diskstats | /usr/bin/awk '{print $6}');
     data2_write_sdb=$(/bin/grep -w sdb /proc/diskstats | /usr/bin/awk '{print $10}');
@@ -33,17 +33,42 @@ if [ "$sdc" ]; then
     read_sdc=$((data2_read_sdc - data1_read_sdc));
     write_sdc=$((data2_write_sdc - data1_write_sdc));
 fi
-if [ "$1" == 'diskonly' ];then
-    /usr/bin/printf "SSD|""R: $(/bin/echo "$((${read_sda2%%}/1024))MB/s") W: $(/bin/echo "$((${write_sda2%%}/1024))MB/s") ||\n";
-else
-    /usr/bin/printf "SSD|""R: $(/bin/echo "$((${read_sda2%%}/1024))MB/s") W: $(/bin/echo "$((${write_sda2%%}/1024))MB/s")\n";
+if [ $read_sda ]; then
+    sda_read_final=$((${read_sda%%}/1024))
 fi
-
+if [ $write_sda ]; then
+    sda_write_final=$((${write_sda%%}/1024))
+fi
+if [ $read_sdb ]; then
+    sdb_read_final=$((${read_sdb%%}/1024))
+fi
+if [ $write_sdb ]; then
+    sdb_write_final=$((${write_sdb%%}/1024))
+fi
+if [ $read_sdc ]; then
+    sdc_read_final=$((${read_sdc%%}/1024))
+fi
+if [ $write_sdcc ]; then
+    sdc_write_final=$((${write_sdc%%}/1024))
+fi
+if [[ "$1" == 'diskonly' ]];then
+    if [[ $sda_read_final -ge 10 || $sda_write_final -ge 10 ]];then
+        /usr/bin/printf "SSD|""R: $(/bin/echo "$sda_read_final MB/s") W: $(/bin/echo "$sda_write_final MB/s") ||\n";
+    fi
+    if [[ $sdb_read_final -ge 10 || $sdb_write_final -ge 10 ]];then
+        /usr/bin/printf "HDD|""R: $(/bin/echo "$sdb_read_final MB/s") W: $(/bin/echo "$sdb_write_final MB/s") ||\n";
+    fi
+    if [[ $sdc_read_final -ge 10 || $sdc_write_final -ge 10  ]];then
+        /usr/bin/printf "sdc|""R: $(/bin/echo "$sdc_read_final MB/s") W: $(/bin/echo "$sdc_write_final MB/s")";
+    fi
+    sleep 1
+    exit
+fi
 if [ "$sdb" ]; then
-    /usr/bin/printf "HDD|""R: $(/bin/echo "$((${read_sdb%%}/1024))MB/s") W: $(/bin/echo "$((${write_sdb%%}/1024))MB/s")\n";
+    /usr/bin/printf "HDD|""R: $(/bin/echo "$sdb_read_final MB/s") W: $(/bin/echo "$sdb_write_final MB/s")\n";
 fi
 if [ "$sdc" ]; then
-    /usr/bin/printf "sdc|""R: $(/bin/echo "$((${read_sdc%%}/1024))MB/s") W: $(/bin/echo "$((${write_sdc%%}/1024))MB/s")\n";
+    /usr/bin/printf "sdc|""R: $(/bin/echo "$sdc_read_final MB/s") W: $(/bin/echo "$sdc_write_final MB/s")\n";
 fi
 if [ "$1" != 'diskonly' ];then
     /usr/bin/printf "CPU Temp: ""$(/bin/echo "$(/usr/bin/sensors | /bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)")""ºc";
