8fe64b51b6b93b3d3b0f61cf5b56ad965d4d0bea
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-10-20 13:29:29 -0300

    atualizando

--- Documentos/scripts/cpu_gpu_temp_taskbar.sh
+++ Documentos/scripts/cpu_gpu_temp_taskbar.sh
@@ -3,7 +3,8 @@ export LANG=C LC_ALL=C;
 if /bin/grep -q 'ENABLED=no' /etc/ufw/ufw.conf; then
     /bin/echo 'Firewall desativado';
 elif [ "$(pgrep easyeffects)" ]; then
-    /usr/bin/printf "EasyEffects ligado | CPU: ""$(/bin/echo "$(/usr/bin/sensors | /bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)")""ºc";
+#     /usr/bin/printf "EasyEffects ligado | CPU: ""$(/bin/echo "$(/usr/bin/sensors | /bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)")""ºc";
+    /usr/bin/printf "EasyEffects ligado";
 else
     if [[ "$(/usr/bin/sensors | /bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)" -ge 65 || "$1" == 'always_show' ]]; then
         /usr/bin/printf "CPU: ""$(/bin/echo "$(/usr/bin/sensors | /bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)")""ºc";
--- Documentos/scripts/disk_monitor_taskbar.sh
+++ Documentos/scripts/disk_monitor_taskbar.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
+sleep=0
 if [ -b "/dev/sdb" ]; then
     sdb="1";
 fi
 if [ -b "/dev/sdc" ]; then
     sdc="1";
 fi
-data1_read_sda2=$(/bin/grep -w sda /proc/diskstats | /usr/bin/awk '{print $6}');
-data1_write_sda2=$(/bin/grep -w sda /proc/diskstats | /usr/bin/awk '{print $10}');
+data1_read_sda=$(/bin/grep -w sda /proc/diskstats | /usr/bin/awk '{print $6}');
+data1_write_sda=$(/bin/grep -w sda /proc/diskstats | /usr/bin/awk '{print $10}');
 if [ "$sdb" ]; then
     data1_read_sdb=$(/bin/grep -w sdb /proc/diskstats | /usr/bin/awk '{print $6}');
     data1_write_sdb=$(/bin/grep -w sdb /proc/diskstats | /usr/bin/awk '{print $10}');
@@ -17,10 +17,10 @@ if [ "$sdc" ]; then
     data1_write_sdc=$(/bin/grep -w sdc /proc/diskstats | /usr/bin/awk '{print $10}');
 fi
 /usr/bin/sleep 0.5 &&
-data2_read_sda2=$(/bin/grep -w sda /proc/diskstats | /usr/bin/awk '{print $6}');
-data2_write_sda2=$(/bin/grep -w sda /proc/diskstats | /usr/bin/awk '{print $10}');
-read_sda=$((data2_read_sda2 - data1_read_sda2));
-write_sda=$((data2_write_sda2 - data1_write_sda2));
+data2_read_sda=$(/bin/grep -w sda /proc/diskstats | /usr/bin/awk '{print $6}');
+data2_write_sda=$(/bin/grep -w sda /proc/diskstats | /usr/bin/awk '{print $10}');
+read_sda=$((data2_read_sda - data1_read_sda));
+write_sda=$((data2_write_sda - data1_write_sda));
 if [ "$sdb" ]; then
     data2_read_sdb=$(/bin/grep -w sdb /proc/diskstats | /usr/bin/awk '{print $6}');
     data2_write_sdb=$(/bin/grep -w sdb /proc/diskstats | /usr/bin/awk '{print $10}');
@@ -51,19 +51,26 @@ fi
 if [ $write_sdcc ]; then
     sdc_write_final=$((${write_sdc%%}/1024))
 fi
+threshold=1
 if [[ "$1" == 'diskonly' ]];then
-    if [[ $sda_read_final -ge 10 || $sda_write_final -ge 10 ]];then
-        /usr/bin/printf "SSD|""R: $(/bin/echo "$sda_read_final MB/s") W: $(/bin/echo "$sda_write_final MB/s") ||\n";
+    if [[ $sda_read_final -ge $threshold || $sda_write_final -ge $threshold ]];then
+        /usr/bin/printf "SSD|""R: $(/bin/echo "$sda_read_final MB/s") W: $(/bin/echo "$sda_write_final MB/s") ";
+        sleep=1
     fi
-    if [[ $sdb_read_final -ge 10 || $sdb_write_final -ge 10 ]];then
-        /usr/bin/printf "HDD|""R: $(/bin/echo "$sdb_read_final MB/s") W: $(/bin/echo "$sdb_write_final MB/s") ||\n";
+    if [[ $sdb_read_final -ge $threshold || $sdb_write_final -ge $threshold ]];then
+        /usr/bin/printf "HDD|""R: $(/bin/echo "$sdb_read_final MB/s") W: $(/bin/echo "$sdb_write_final MB/s") ";
+        sleep=1
     fi
-    if [[ $sdc_read_final -ge 10 || $sdc_write_final -ge 10  ]];then
+    if [[ $sdc_read_final -ge $threshold || $sdc_write_final -ge $threshold  ]];then
         /usr/bin/printf "sdc|""R: $(/bin/echo "$sdc_read_final MB/s") W: $(/bin/echo "$sdc_write_final MB/s")";
+        sleep=1
     fi
-    sleep 1
+    if [ "$sleep" == '1' ];then
+        sleep 1;
+    fi
     exit
 fi
+/usr/bin/printf "SSD|""R: $(/bin/echo "$sda_read_final MB/s") W: $(/bin/echo "$sda_write_final MB/s")\n";
 if [ "$sdb" ]; then
     /usr/bin/printf "HDD|""R: $(/bin/echo "$sdb_read_final MB/s") W: $(/bin/echo "$sdb_write_final MB/s")\n";
 fi
