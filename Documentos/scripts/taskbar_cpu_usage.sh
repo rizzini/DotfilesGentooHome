5016e6bc8c53aed8f802d4026e9239119b60b7a3
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-10-27 10:27:27 -0300

    atualizando

--- Documentos/scripts/taskbar_cpu_usage.sh
+++ Documentos/scripts/taskbar_cpu_usage.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-export LANG=C LC_ALL=C
+command='if [ "$(pgrep "systemmonitor")" ];then killall systemmonitor &> /dev/null;else /usr/bin/systemmonitor & disown $!;fi'
 while :; do
-    DATA=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1); }' <(grep 'cpu ' /proc/stat) <(sleep 1;grep 'cpu ' /proc/stat))
+    cpu_usage=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1); }' <(grep 'cpu ' /proc/stat) <(sleep 1;grep 'cpu ' /proc/stat))
+    cpu_usage=${cpu_usage%.*}
+    DATA='| A | CPU: <b>'$cpu_usage'%</b> | | '$command' |'
     if [ "$DATA" != "$DATA_last" ];then
-        /usr/bin/qdbus org.kde.plasma.doityourselfbar /id_954 org.kde.plasma.doityourselfbar.pass '| A | CPU: <b>'${DATA%.*}'%</b> | | |'
+        /usr/bin/qdbus org.kde.plasma.doityourselfbar /id_954 org.kde.plasma.doityourselfbar.pass "$DATA"
         DATA_last="$DATA"
     fi
     sleep 0.5
