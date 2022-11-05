69966f6727ea95429cc749103b88d4d10d917abf
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-11-05 15:44:09 -0300

    atualizando

--- Documentos/scripts/taskbar_cpu_usage_temp.sh
+++ Documentos/scripts/taskbar_cpu_usage_temp.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 command='if [ "$(pgrep "htop")" ];then /usr/bin/killall htop;else /usr/bin/alacritty -o window.dimensions.lines=33 window.dimensions.columns=120 -e /usr/bin/htop;fi'
+threshold=70
 while :; do
+    cpu_temp=$(/usr/bin/sensors | /bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)
     cpu_usage=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1); }' <(grep 'cpu ' /proc/stat) <(sleep 1;grep 'cpu ' /proc/stat))
     cpu_usage=${cpu_usage%.*}
-    DATA='| A | CPU: <b>'$cpu_usage'%</b> | | '$command' |'
+    if [ $cpu_temp -ge $threshold ]; then
+        DATA='| C | CPU: <b>'$cpu_usage'%</b> \| Temp: <b>'$cpu_temp'ºc</b> | | '$command' |'
+    else
+        DATA='| A | CPU: <b>'$cpu_usage'%</b> | Temp: <b>'$cpu_temp'ºc</b> | '$command' |'
+    fi
     if [ "$DATA" != "$DATA_last" ];then
         /usr/bin/qdbus org.kde.plasma.doityourselfbar /id_954 org.kde.plasma.doityourselfbar.pass "$DATA"
         DATA_last="$DATA"
     fi
     sleep 0.5
 done
-
--- Documentos/scripts/taskbar_memory_usage.sh
+++ Documentos/scripts/taskbar_memory_usage.sh
@@ -30,30 +30,24 @@ size () {
     echo "${whole}${decimal}${units[$unit]}"
 }
 command='if [ "$(pgrep "intel_gpu_top")" ];then /usr/bin/sudo /usr/bin/killall intel_gpu_top;else /usr/bin/alacritty -o window.dimensions.lines=13 window.dimensions.columns=55 -e /usr/bin/sudo /usr/bin/intel_gpu_top;fi'
-threshold=70
 while :;do
     mem_stats=()
     mem_stats+=($(/bin/grep -e "MemTotal" -e "MemAvailable" -e 'SwapTotal' -e 'SwapFree' /proc/meminfo))
     mem_used=$(((mem_stats[1] - mem_stats[4]) - 256000))
     swap_used=$((mem_stats[7] - mem_stats[10]))
-    cpu_temp=$(/usr/bin/sensors | /bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)
     if /bin/grep -q 'ENABLED=no' /etc/ufw/ufw.conf; then
-        DATA='| C | Firewall <b>desativado</b> \| RAM: <b>'$(size $mem_used)'</b> \| Swap: <b>'$(size $swap_used)'</b> | CPU temp: <b>'$cpu_temp'ºc</b> | '$command' |';
+        DATA='| C | Firewall <b>desativado</b> \| RAM: <b>'$(size $mem_used)'</b> \| Swap: <b>'$(size $swap_used)'</b> | | '$command' |';
     elif [ "$(/usr/bin/pgrep easyeffects)" ]; then
         if [ $mem_used -ge 6000000 ];then
-            DATA='| B | EasyEffects <b>ligado</b> \| RAM: <b>'$(size $mem_used)'</b> \| Swap: <b>'$(size $swap_used)'</b> | CPU temp: <b>'$cpu_temp'ºc</b> | '$command' |'
-        elif [ $cpu_temp -ge $threshold ]; then
-            DATA='| C | EasyEffects <b>ligado</b> \| CPU <b>'$cpu_temp'ºc</b> | Consumo RAM: <b>'$(size $mem_used)'</b> \| Swap: <b>'$(size $swap_used)'</b> | '$command' |'
+            DATA='| B | EasyEffects <b>ligado</b> \| RAM: <b>'$(size $mem_used)'</b> \| Swap: <b>'$(size $swap_used)'</b> | | '$command' |'
         else
-            DATA='| A | EasyEffects <b>ligado</b> \| RAM: <b>'$(size $mem_used)'</b> \| Swap: <b>'$(size $swap_used)'</b> | CPU temp: <b>'$cpu_temp'ºc</b> | '$command' |'
+            DATA='| A | EasyEffects <b>ligado</b> \| RAM: <b>'$(size $mem_used)'</b> \| Swap: <b>'$(size $swap_used)'</b> | | '$command' |'
         fi
     else
-        if [ $cpu_temp -ge $threshold ]; then
-            DATA='| C | CPU <b>'$cpu_temp'ºc</b> \| Consumo RAM: <b>'$(size $mem_used)'</b> \| Swap: <b>'$(size $swap_used)'</b> | | '$command' |'
-        elif [ $mem_used -ge 6000000 ];then
-            DATA='| B | Consumo RAM: <b>'$(size $mem_used)'</b> \| Swap: <b>'$(size $swap_used)'</b> | CPU temp: <b>'$cpu_temp'ºc</b> | '$command' |'
+        if [ $mem_used -ge 6000000 ];then
+            DATA='| B | Consumo RAM: <b>'$(size $mem_used)'</b> \| Swap: <b>'$(size $swap_used)'</b> | | '$command' |'
         else
-            DATA='| A | Consumo RAM: <b>'$(size $mem_used)'</b> \| Swap: <b>'$(size $swap_used)'</b> | CPU temp: <b>'$cpu_temp'ºc</b> | '$command' |'
+            DATA='| A | Consumo RAM: <b>'$(size $mem_used)'</b> \| Swap: <b>'$(size $swap_used)'</b> | | '$command' |'
         fi
     fi
     if [ "$DATA" != "$DATA_last" ];then
