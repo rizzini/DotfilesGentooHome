a5e90bf02b34b868d1e1e94097ff47eab3df24cd
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-11-07 18:53:18 -0300

    atualizando

--- Documentos/scripts/taskbar_disk_monitor.sh
+++ Documentos/scripts/taskbar_disk_monitor.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 export LANG=C LC_ALL=C;
 declare -A data1_read data1_write read write threshold show counter counter_no_data;
-show_data_seconds=5;
 command='if [ "$(pgrep "systemmonitor")" ];then killall systemmonitor &> /dev/null;else /usr/bin/systemmonitor & disown $!;fi';
 while :; do
     readarray -t disk_list < <(/usr/bin/awk '(!/[0-9]$/) && (NR>2) {print $4}' /proc/partitions);
-    for disk in "${disk_list[@]}"; do
-        if [[ "$(/bin/lsblk -do name,tran | /bin/grep "$disk")" == *"usb"* ]]; then
-            threshold[$disk]=1;
-        else
-            threshold[$disk]=10;
-        fi
-    done
     DATA=();
     has_data=();
     for disk in "${disk_list[@]}"; do
         counter[$disk]=$((counter[$disk]+1));
         data1_read[$disk]=$(/usr/bin/awk '/\<'"$disk"'\>/{print $6}' /proc/diskstats);
         data1_write[$disk]=$(/usr/bin/awk '/\<'"$disk"'\>/{print $10}' /proc/diskstats);
-        /usr/bin/sleep 0.5 &&
+    done
+    /usr/bin/sleep 0.5;
+    for disk in "${disk_list[@]}"; do
         data2_read[$disk]=$(/usr/bin/awk '/\<'"$disk"'\>/{print $6}' /proc/diskstats);
         data2_write[$disk]=$(/usr/bin/awk '/\<'"$disk"'\>/{print $10}' /proc/diskstats);
         read[$disk]=$((data2_read[$disk] - data1_read[$disk]));
         write[$disk]=$((data2_write[$disk] - data1_write[$disk]));
         read[$disk]=$((${read[$disk]%%}/1024));
         write[$disk]=$((${write[$disk]%%}/1024));
+        if [[ "$(/bin/lsblk -do name,tran | /bin/grep "$disk")" == *"usb"* ]]; then
+            threshold[$disk]=1;
+        else
+            threshold[$disk]=10;
+        fi
         if [[ ${read[$disk]} -ge ${threshold[$disk]} || ${write[$disk]} -ge ${threshold[$disk]} || ${show[$disk]} -eq 1 ]]; then
             DATA+='| A | '${disk}'\| R: <b>'${read[$disk]}'MB/s</b> W: <b>'${write[$disk]}'MB/s</b> | | '$command' |';
             has_data+=("$disk");
@@ -41,7 +40,7 @@ while :; do
         fi
     done
     for disk in "${disk_list[@]}"; do
-        if [ $((counter_no_data[$disk]+show_data_seconds)) == ${counter[$disk]} ]; then
+        if [ $((counter_no_data[$disk]+9)) == ${counter[$disk]} ]; then
             show[$disk]=0;
             counter[$disk]=0;
             counter_no_data[$disk]=0;
@@ -50,7 +49,7 @@ while :; do
     if [ "$DATA" != "$DATA_last" ];then
         /usr/bin/qdbus org.kde.plasma.doityourselfbar /id_951 org.kde.plasma.doityourselfbar.pass "${DATA[@]}";
         DATA_last="$DATA";
-        /usr/bin/sleep 0.5;
+        /usr/bin/sleep 0.3;
     else
         /usr/bin/sleep 2;
     fi
