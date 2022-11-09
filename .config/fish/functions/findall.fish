051cd21ea6f527fd21de673c3e745403ab94d291
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-11-09 07:25:32 -0300

    atualizando

--- .config/fish/functions/findall.fish
+++ .config/fish/functions/findall.fish
@@ -1,13 +1,3 @@
 function findall
-    sudo find / -not \( -wholename "/mnt" -prune -o -wholename "/usr/share/locale" -prune -o -wholename "/home/lucas/.local/share/icons" -prune -o -wholename "/usr/share/man" -prune -o -wholename "/var/db/repos" -prune \)  -iname """*$argv[1]*""" 2> /dev/null
+    sudo find / -not \( -wholename "/mnt" -prune -o -wholename "/usr/share/locale" -prune -o -wholename "/home/lucas/.local/share/icons" -prune -o -wholename "/usr/share/man" -prune -o -wholename "/var/db/repos" -prune -o -wholename "/ccache" -prune \)  -iname """*$argv[1]*""" 2> /dev/null
 end
-
-
-
-
-
-
-
-
-
-
