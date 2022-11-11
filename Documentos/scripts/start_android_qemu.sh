09566a402d19aa0f6e869658a5df020d51116cb3
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-11-11 16:10:27 -0300

    atualizando

--- Documentos/scripts/start_android_qemu.sh
+++ Documentos/scripts/start_android_qemu.sh
@@ -12,19 +12,16 @@ IPV4_BROADCAST="0.0.0.0"
 IPV4_NAT="true"
 use_iptables_lock="-w"
 iptables -w -L -n > /dev/null 2>&1 || use_iptables_lock=""
-
 _netmask2cidr () {
     local x=${1##*255.}
     set -- "0^^^128^192^224^240^248^252^254^" "$(( (${#1} - ${#x})*2 ))" "${x%%.*}"
     x=${1%%${3}*}
     echo $(( ${2} + (${#x}/4) ))
 }
-
 ifdown() {
     ip addr flush dev "${1}"
     ip link set dev "${1}" down
 }
-
 ifup() {
     [ "${HAS_IPV6}" = "true" ] && [ "${IPV6_PROXY}" = "true" ] && ip addr add fe80::1/64 dev "${1}"
     if [ -n "${IPV4_NETMASK}" ] && [ -n "${IPV4_ADDR}" ]; then
@@ -34,21 +31,18 @@ ifup() {
     fi
     ip link set dev "${1}" up
 }
-
 start() {
-    [ ! -f "${varrun}/network_up" ] || [ "${1}" = "force" ] || { echo "android_bridge0 is already running"; exit 1; }
     if [ -d /sys/class/net/${BRIDGE} ]; then
-        stop force 2>/dev/null || true
+        stop 2>/dev/null || true
     fi
     FAILED=1
     cleanup() {
         set +e
         if [ "${FAILED}" = "1" ]; then
             echo "Failed to setup android_bridge0." >&2
-            stop force
+            stop
         fi
     }
-#     trap cleanup EXIT HUP INT TERM
     set -e
     [ ! -d "/sys/class/net/${BRIDGE}" ] && ip link add dev "${BRIDGE}" type bridge
     if [ ! -d "${varrun}" ]; then
@@ -62,41 +56,38 @@ start() {
     if [ -n "${IPV4_ADDR}" ] && [ -n "${IPV4_NETMASK}" ] && [ -n "${IPV4_NETWORK}" ]; then
         echo 1 > /proc/sys/net/ipv4/ip_forward
         if [ "${IPV4_NAT}" = "true" ]; then
-            iptables "${use_iptables_lock}" -t nat -A POSTROUTING -s "${IPV4_NETWORK}" ! -d "${IPV4_NETWORK}" -j MASQUERADE -m comment --comment "android_bridge0 bridge do meu_android"
+            iptables "${use_iptables_lock}" -t nat -A POSTROUTING -s "${IPV4_NETWORK}" ! -d "${IPV4_NETWORK}" -j MASQUERADE
         fi
     fi
-    iptables "${use_iptables_lock}" -I INPUT -i "${BRIDGE}" -p udp --dport 67 -j ACCEPT -m comment --comment "android_bridge0 bridge do meu_android"
-    iptables "${use_iptables_lock}" -I INPUT -i "${BRIDGE}" -p tcp --dport 67 -j ACCEPT -m comment --comment "android_bridge0 bridge do meu_android"
-    iptables "${use_iptables_lock}" -I INPUT -i "${BRIDGE}" -p udp --dport 53 -j ACCEPT -m comment --comment "android_bridge0 bridge do meu_android"
-    iptables "${use_iptables_lock}" -I INPUT -i "${BRIDGE}" -p tcp --dport 53 -j ACCEPT -m comment --comment "android_bridge0 bridge do meu_android"
-    iptables "${use_iptables_lock}" -I FORWARD -i "${BRIDGE}" -j ACCEPT -m comment --comment "android_bridge0 bridge do meu_android"
-    iptables "${use_iptables_lock}" -I FORWARD -o "${BRIDGE}" -j ACCEPT -m comment --comment "android_bridge0 bridge do meu_android"
-    iptables "${use_iptables_lock}" -t mangle -A POSTROUTING -o "${BRIDGE}" -p udp -m udp --dport 68 -j CHECKSUM --checksum-fill -m comment --comment "android_bridge0 bridge do meu_android"
+    iptables "${use_iptables_lock}" -I INPUT -i "${BRIDGE}" -p udp --dport 67 -j ACCEPT
+    iptables "${use_iptables_lock}" -I INPUT -i "${BRIDGE}" -p tcp --dport 67 -j ACCEPT
+    iptables "${use_iptables_lock}" -I INPUT -i "${BRIDGE}" -p udp --dport 53 -j ACCEPT
+    iptables "${use_iptables_lock}" -I INPUT -i "${BRIDGE}" -p tcp --dport 53 -j ACCEPT
+    iptables "${use_iptables_lock}" -I FORWARD -i "${BRIDGE}" -j ACCEPT
+    iptables "${use_iptables_lock}" -I FORWARD -o "${BRIDGE}" -j ACCEPT
+    iptables "${use_iptables_lock}" -t mangle -A POSTROUTING -o "${BRIDGE}" -p udp -m udp --dport 68 -j CHECKSUM --checksum-fill
     touch "${varrun}/network_up"
     FAILED=0
 }
-
 stop() {
-    [ -f "${varrun}/network_up" ] || [ "${1}" = "force" ] || { echo "android_bridge0 isn't running"; exit 1; }
     if [ -d /sys/class/net/${BRIDGE} ]; then
         ifdown ${BRIDGE}
-        iptables ${use_iptables_lock} -D INPUT -i ${BRIDGE} -p udp --dport 67 -j ACCEPT -m comment --comment "android_bridge0 bridge do meu_android"
-        iptables ${use_iptables_lock} -D INPUT -i ${BRIDGE} -p tcp --dport 67 -j ACCEPT -m comment --comment "android_bridge0 bridge do meu_android"
-        iptables ${use_iptables_lock} -D INPUT -i ${BRIDGE} -p udp --dport 53 -j ACCEPT -m comment --comment "android_bridge0 bridge do meu_android"
-        iptables ${use_iptables_lock} -D INPUT -i ${BRIDGE} -p tcp --dport 53 -j ACCEPT -m comment --comment "android_bridge0 bridge do meu_android"
-        iptables ${use_iptables_lock} -D FORWARD -i ${BRIDGE} -j ACCEPT -m comment --comment "android_bridge0 bridge do meu_android"
-        iptables ${use_iptables_lock} -D FORWARD -o ${BRIDGE} -j ACCEPT -m comment --comment "android_bridge0 bridge do meu_android"
-        iptables ${use_iptables_lock} -t mangle -D POSTROUTING -o ${BRIDGE} -p udp -m udp --dport 68 -j CHECKSUM --checksum-fill -m comment --comment "android_bridge0 bridge do meu_android"
+        iptables ${use_iptables_lock} -D INPUT -i ${BRIDGE} -p udp --dport 67 -j ACCEPT
+        iptables ${use_iptables_lock} -D INPUT -i ${BRIDGE} -p tcp --dport 67 -j ACCEPT
+        iptables ${use_iptables_lock} -D INPUT -i ${BRIDGE} -p udp --dport 53 -j ACCEPT
+        iptables ${use_iptables_lock} -D INPUT -i ${BRIDGE} -p tcp --dport 53 -j ACCEPT
+        iptables ${use_iptables_lock} -D FORWARD -i ${BRIDGE} -j ACCEPT
+        iptables ${use_iptables_lock} -D FORWARD -o ${BRIDGE} -j ACCEPT
+        iptables ${use_iptables_lock} -t mangle -D POSTROUTING -o ${BRIDGE} -p udp -m udp --dport 68 -j CHECKSUM --checksum-fill
         if [ -n "${IPV4_NETWORK}" ] && [ "${IPV4_NAT}" = "true" ]; then
-            iptables ${use_iptables_lock} -t nat -D POSTROUTING -s ${IPV4_NETWORK} ! -d ${IPV4_NETWORK} -j MASQUERADE -m comment --comment "android_bridge0 bridge do meu_android"
+            iptables ${use_iptables_lock} -t nat -D POSTROUTING -s ${IPV4_NETWORK} ! -d ${IPV4_NETWORK} -j MASQUERADE
         fi
         ls /sys/class/net/${BRIDGE}/brif/* > /dev/null 2>&1 || ip link delete "${BRIDGE}"
     fi
     rm -f "${varrun}/network_up"
 }
-
 if ! pgrep -f -- 'qemu-system-x86_64 -name Android'; then
-    start force
+    start
     for i in $(lsusb -d '1908:2310' | awk '{print $2}{print $4}' | tr -d ':'); do
         webcam+=("$i");
     done
@@ -130,8 +121,8 @@ if ! pgrep -f -- 'qemu-system-x86_64 -name Android'; then
     while pgrep -f -- 'qemu-system-x86_64 -name Android'; do
         sleep 3;
     done
-    stop force
+    stop
 else
     pkill -f 'qemu-system-x86_64 -name Android'
-    stop force
+    stop
 fi
