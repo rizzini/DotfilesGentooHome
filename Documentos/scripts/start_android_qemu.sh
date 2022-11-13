096cbcdda9366b5d918999e9507e95cb5c98235d
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-11-13 18:14:34 -0300

    atualizando

--- Documentos/scripts/start_android_qemu.sh
+++ Documentos/scripts/start_android_qemu.sh
@@ -55,17 +55,12 @@ if ! pgrep -f 'qemu-system-i386 -name Android'; then
                         -nodefaults \
                         -machine vmport=off \
                         -audiodev pa,id=pa -audio pa,model=es1370 \
-                        -usbdevice tablet \
+                        -usbdevice tablet -usbdevice keyboard \
                         -netdev bridge,id=hn0,br=android_bridge0 -device virtio-net-pci,netdev=hn0,id=nic1 \
                         -device qemu-xhci,id=xhci -device usb-host,hostdevice=/dev/bus/usb/'"${webcam[0]}"'/'"${webcam[1]}"' \
                         -device virtio-vga-gl \
-                        -display gtk,gl=on,show-cursor=on,show-menubar=off \
-                        -drive file=/home/lucas/.android/androidx86_hda.img,if=virtio' &
-    if [ "$?" != '0' ];then
-        /bin/machinectl shell --uid=lucas .host /usr/bin/notify-send -u critical "Erro ao executar a VM..";
-        stop_bridge
-        exit 1;
-    fi
+                        -display gtk,gl=on,show-cursor=on,show-menubar=off,zoom-to-fit=off \
+                        -drive file=/home/lucas/.android/androidx86_hda.img,if=virtio,cache=off' &
     sleep 13;
     ok=0
     while pgrep -f 'qemu-system-i386 -name Android'; do
