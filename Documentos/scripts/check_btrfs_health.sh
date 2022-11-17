c17b8d1cb0313b77117861d568afb679af38b2f2
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-11-17 14:21:09 -0300

    atualizando

--- Documentos/scripts/check_btrfs_health.sh
+++ Documentos/scripts/check_btrfs_health.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 if [ "$EUID" -ne 0 ]; then
-    /bin/echo "Please run as root";
+    /bin/echo "No root, no deal..";
     exit;
 fi
 if test -e "/tmp/check_btrfs_health.sh.tmp"; then
     exit;
 fi
 /usr/bin/touch "/tmp/check_btrfs_health.sh.tmp";
-if ! /sbin/btrfs device stats -c /; then
-    /sbin/btrfs device stats -c / | /usr/bin/tee -a /home/lucas/check_btrfs_health.sh.ssd.log;
-    /bin/machinectl shell --uid=lucas .host /usr/bin/notify-send -u critical "Erro de E/S: SSD | Checar ~/check_btrfs_health.sh.ssd.log";
-fi
-if /bin/mount | /bin/grep -q '/mnt/dados'; then
-    if [ ! "$(/sbin/btrfs device stats -c /mnt/dados)" ]; then
-        /sbin/btrfs device stats -c /mnt/dados | /usr/bin/tee -a /home/lucas/check_btrfs_health.sh.mnt.dados.log;
-        /bin/machinectl shell --uid=lucas .host /usr/bin/notify-send -u critical "Erro de E/S: /mnt/dados/ | Checar ~/check_btrfs_health.sh.mnt.dados.log";
+for path in $(mount | grep btrfs | awk '{print $3}'); do
+    if ! /sbin/btrfs device stats $path; then
+        /bin/machinectl shell --uid=lucas .host /usr/bin/notify-send -u critical "Erro de E/S: "$path"";
     fi
-fi
-if /bin/mount | /bin/grep -q '/mnt/backup'; then
-    if ! /sbin/btrfs device stats -c /mnt/backup; then
-        /sbin/btrfs device stats -c /mnt/backup | /usr/bin/tee -a /home/lucas/check_btrfs_health.sh.mnt.backup.log;
-        /bin/machinectl shell --uid=lucas .host /usr/bin/notify-send -u critical "Erro de E/S: /mnt/backup/ | Checar ~/check_btrfs_health.sh.mnt.backup.log";
-    fi
-fi
+done
 /bin/rm -f "/tmp/check_btrfs_health.sh.tmp";
