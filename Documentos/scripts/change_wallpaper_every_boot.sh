35f77991f525e63f22772ed95af379cee25d4d0d
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-11-18 22:33:39 -0300

    atualizando

--- Documentos/scripts/change_wallpaper_every_boot.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/kwriteconfig5 --file=/home/lucas/.config/plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 192 --group Wallpaper --group org.kde.image --group General --key Image "file://$(/usr/bin/find /home/lucas/Imagens/Wallpaper/ -type f -print0 | /usr/bin/xargs -0 file --mime-type | /usr/bin/grep -F 'image/' | /usr/bin/cut -d ':' -f 1 | /usr/bin/sort -R | /usr/bin/head -n 1)"
